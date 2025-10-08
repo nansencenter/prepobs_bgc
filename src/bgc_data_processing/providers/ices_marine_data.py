0219d74d58eaee04d4d5d7897cd693ebcb0e8d29
Tsuyoshi Wakamatsu <56160789+tsuyoshiwakamatsu@users.noreply.github.com>
2025-10-08 13:10:45 +0200

    ICES_MARINE_DATA loader added

---
+++ src/bgc_data_processing/providers/ices_marine_data.py
@@ -0,0 +1,46 @@
+"""Specific parameters to load ICES-provided data."""
+
+from pathlib import Path
+
+from bgc_data_processing import units
+from bgc_data_processing.core.sources import DataSource
+from bgc_data_processing.core.variables.sets import SourceVariableSet
+from bgc_data_processing.defaults import PROVIDERS_CONFIG, VARS
+from bgc_data_processing.utils.patterns import FileNamePattern
+
+loader = DataSource(
+    provider_name="ICES",
+    data_format="csv",
+    dirin=Path(PROVIDERS_CONFIG["ICES"]["PATH"]),
+    data_category=PROVIDERS_CONFIG["ICES"]["CATEGORY"],
+    excluded_files=PROVIDERS_CONFIG["ICES"]["EXCLUDE"],
+    files_pattern=FileNamePattern("ices_marine_data_{years}.csv"),
+    variable_ensemble=SourceVariableSet(
+        provider=VARS["provider"].not_in_file(),
+        expocode=VARS["expocode"].in_file_as("Cruise"),
+        date=VARS["date"].in_file_as("Date"),
+        year=VARS["year"].not_in_file(),
+        month=VARS["month"].not_in_file(),
+        day=VARS["day"].not_in_file(),
+        hour=VARS["hour"].not_in_file(),
+        longitude=VARS["longitude"].in_file_as("Longitude"),
+        latitude=VARS["latitude"].in_file_as("Latitude"),
+        depth=VARS["depth"]
+        .in_file_as("Bot. Depth")
+        .remove_when_nan()
+        .correct_with(lambda x: -x),
+        temperature=VARS["temperature"].in_file_as("Temperature"),
+        salinity=VARS["salinity"].in_file_as("Salinity"),
+        oxygen=VARS["oxygen"]
+        .in_file_as("Oxygen")
+        .correct_with(units.convert_doxy_ml_by_l_to_mmol_by_m3),
+        phosphate=VARS["phosphate"].in_file_as("Phosphate").remove_when_all_nan(),
+        nitrate=VARS["nitrate"].in_file_as("Nitrate").remove_when_all_nan(),
+        silicate=VARS["silicate"].in_file_as("Silicate").remove_when_all_nan(),
+        chlorophyll=VARS["chlorophyll"].in_file_as("Chlorophyll a").remove_when_all_nan(),
+    ),
+    read_params={
+        "low_memory": False,
+        "skiprows": [1],
+    },
+)
